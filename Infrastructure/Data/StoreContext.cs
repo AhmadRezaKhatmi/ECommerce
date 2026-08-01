b8c6508180b6bbe914e03869e8ef7864cf094190
ah-khatmi <ah-khatmi@88077-T2SH>
2026-08-01 16:01:33 +0330

    Add DBContext

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
