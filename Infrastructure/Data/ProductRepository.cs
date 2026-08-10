8304dd613a6a161d84cc316f28bc5a2710665892
ah-khatmi <ah-khatmi@88077-T2SH>
2026-08-10 14:54:02 +0330

    Add Types & Brands

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -23,6 +23,13 @@ namespace Infrastructure.Data
             context.Products.Remove(product);
         }
 
+        public async Task<IReadOnlyList<string>> GetBrandsAsync()
+        {
+            return await context.Products.Select(p => p.Brand)
+                .Distinct()
+                .ToListAsync();
+        }
+
         public async Task<Product?> GetProductByIdAsync(int id)
         {
             return await context.Products.FindAsync(id);
@@ -31,6 +38,13 @@ namespace Infrastructure.Data
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
             return await context.Products.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<string>> GetTypesAsync()
+        {
+            return await context.Products.Select(p => p.Type)
+              .Distinct()
+              .ToListAsync();
         }
 
         public bool ProductExists(int id)
