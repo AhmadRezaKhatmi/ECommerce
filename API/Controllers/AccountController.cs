6a0a60ce817cdd5a831caf15bbffd9bd7d95d149
ah-khatmi <ah-khatmi@88077-T2SH>
2026-09-26 14:49:02 +0330

    Update Register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,7 +26,14 @@ namespace API.Controllers
             var result = await signInManager.UserManager.CreateAsync(user, registerDTO.Password);
 
             if (!result.Succeeded)
-                return BadRequest(result.Errors);
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            }
 
             return Ok();
 
