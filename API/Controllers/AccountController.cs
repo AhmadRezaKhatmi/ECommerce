aaa2b86fb5bdfaa4e64cd90ce743269f337dfc7c
ah-khatmi <ah-khatmi@88077-T2SH>
2026-09-26 14:38:55 +0330

    Add Extensions

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using API.DTOs;
+using API.Extensions;
 using Core.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -78,8 +79,7 @@ namespace API.Controllers
             if (User.Identity?.IsAuthenticated == false)
                 return NoContent();
 
-            var user = await signInManager.UserManager.Users.
-                FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+            var user = await signInManager.UserManager.GetUserByEmail(User);
 
             if (user == null)
                 return Unauthorized();
